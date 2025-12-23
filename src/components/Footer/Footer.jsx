ab820daf766c4795b6a6e3c29e82dc258aafc0c6
Delaram Farzad <delaramfarzad9@gmail.com>
2025-12-23 23:45:29 +0330

    footer hover

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <span>© 2025 My Weather App</span>
       
       <div className="flex gap-4">
-        <a href="https://github.com/delaramfarzad9/weather-app" className="hover:text-rose-500">GitHub</a>
+        <a href="https://github.com/delaramfarzad9/weather-app" className="hover:text-yellow-500">GitHub</a>
         {/* <a href="https://yourportfolio.com" className="hover:text-rose-500">Portfolio</a> */}
       </div>
     </footer>
